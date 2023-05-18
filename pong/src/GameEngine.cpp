73aee36cda535630338780f23304a5ed2b023627
JustinLTH <justinlth3@gmail.com>
2023-05-18 12:18:19 +0100

    player move paddle

--- pong/src/GameEngine.cpp
+++ pong/src/GameEngine.cpp
@@ -1,9 +1,9 @@
 #include "GameEngine.h"
 
-GameEngine::GameEngine(sf::RenderWindow& window) 
+GameEngine::GameEngine(sf::RenderWindow& window)
 	: m_window(window),
 	m_paddle1(sf::Vector2f(20, window.getSize().y / 2.f), 10, 100, sf::Color::White),
-	m_paddle2(sf::Vector2f(window.getSize().x - 20.f, window.getSize().y -100.f), 10, 100, sf::Color::White),
+	m_paddle2(sf::Vector2f(window.getSize().x - 20.f, window.getSize().y - 100.f), 10, 100, sf::Color::White),
 	m_ball(sf::Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f), 8, 400.f, sf::Color::White)
 {
 	m_p1Score = 0;
@@ -17,7 +17,7 @@ GameEngine::GameEngine(sf::RenderWindow& window)
 	m_hud.setPosition((m_window.getSize().x / 2.f) - 45.f, 10);
 
 	m_paddle1.setSpeed(1000.f);
-	
+
 }
 
 void GameEngine::draw()
@@ -36,24 +36,26 @@ void GameEngine::update()
 	std::stringstream ss;
 	switch (m_gStates)
 	{
-	case GameEngine::intro:
-		ss << "Press the Space\nkey to start";
-		break;
-	case GameEngine::playing:
-		ss << m_p1Score << " - " << m_p2Score;
-		break;
-	case GameEngine::gameOver:
-		if (m_p1Score > m_p2Score) {
-			ss << "Player 1 wins";
-		}
-		else {
-			ss << "Player 2 wins";
-		}
-		break;
-	default:
-		break;
+		case GameEngine::intro:
+			ss << "Press the Space\nkey to start";
+			break;
+		case GameEngine::playing:
+			ss << m_p1Score << " - " << m_p2Score;
+			break;
+		case GameEngine::gameOver:
+			if (m_p1Score > m_p2Score)
+			{
+				ss << "Player 1 wins";
+			}
+			else
+			{
+				ss << "Player 2 wins";
+			}
+			break;
+		default:
+			break;
 	}
-		
+
 	m_hud.setString(ss.str());
 }
 
@@ -76,8 +78,12 @@ void GameEngine::run()
 		}
 
 		// ADD YOUR CODE HERE !!!
-		
-		
+		if (m_gStates == GameStates::playing)
+		{
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))m_paddle1.move(dt, 0);
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))m_paddle1.move(dt, m_window.getSize().y);
+		}
+
 		// update hud
 		update();
 		// draw shapes to screen
